1eef2541ff5adae58d95b79465fbbd153d337986
Subitha07 <subithamurugan87@gmail.com>
2025-01-08 18:42:35 +0530

    React News App

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+
+
+function Navbar({setCategory}) {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary"  data-bs-theme="dark">
+
+    <div className="container-fluid ">
+
+      <a className="navbar-brand" href="#"><span className="badge bg-light text-dark fs-4">News App</span></a>
+
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav text ms-auto me-5 ">
+          <li className="nav-item p-3">
+            <div className="nav-link" onClick={()=>setCategory("technology")} >Technology</div>
+          </li>
+          <li className="nav-item p-3">
+            <div className="nav-link"  onClick={()=>setCategory("Business")} >Business</div>
+          </li>
+          <li className="nav-item p-3">
+            <div className="nav-link"  onClick={()=>setCategory("Health")} >Health</div>
+          </li>
+          <li className="nav-item p-3">
+            <div className="nav-link"  onClick={()=>setCategory("Sports")} >Sports</div>
+          </li>
+          <li className="nav-item p-3">
+            <div className="nav-link"  onClick={()=>setCategory("Entertainment")} >Entertainment</div>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/NewsItem.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import news_image from "../assets/news.jpg";
+
+function NewsItem({ title, description, src, url }) {
+  return (
+    <div
+      className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2 "
+      style={{ maxWidth: "345px" }}
+    >
+      <img
+        src={src ? src : news_image}
+        style={{ height: "200px", width: "325px" }}
+        className="card-img-top"
+        alt="..."
+      />
+      <div className="card-body">
+        <h5 className="card-title">{title.slice(0, 50)}</h5>
+        <p className="card-text">
+          {description
+            ? description.slice(0, 90)
+            : "News is a report of a current event. It is information about something that has just happened"}
+        </p>
+        <a href={url} class="btn btn-primary">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+}
+ 
+export default NewsItem;
